45db44abc4cf45851bb7b2299a3fa8962baa67de
Victor Hugo <111150704+VictorHugoSimon@users.noreply.github.com>
2026-08-20 15:16:20 -0300

    db: version contract migration

---
+++ migrations/2026-08-20-contrato.sql
@@ -0,0 +1,7 @@
+ALTER TABLE companies ADD COLUMN billing_to TEXT;
+ALTER TABLE companies ADD COLUMN billing_email TEXT;
+ALTER TABLE companies ADD COLUMN billing_amount TEXT;
+ALTER TABLE companies ADD COLUMN billing_day TEXT;
+ALTER TABLE companies ADD COLUMN stakeholders TEXT;
+ALTER TABLE projects ADD COLUMN stakeholders TEXT;
+ALTER TABLE projects ADD COLUMN billing_to TEXT;
